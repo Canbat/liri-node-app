e1e6065374246dc3b766295d690e61de0ad0f1e6
Canbat <36976030+Canbat@users.noreply.github.com>
2018-04-20 23:24:05 -0400

    Add files via upload

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log("Keys loaded");
+
+var twitterKeys = {
+  consumer_key: "JkOvJkXg5YfOk6tbxyDeuTVeu",
+  consumer_secret: "dLsnB70Y1UoABOEkMAqJ3pUoG5d5SzRjELp61CzjPFEJgquco3",
+    access_token_key: "987523969971638272-JdT7qFkdzUCy3h17mdmSEGmvI5sFCD3",
+  access_token_secret: "p02NzZflFNR0UoeoInCnR9bVi0oRLda9fb1mUjaxvYaWD"
+};
+
+module.exports = twitterKeys;
